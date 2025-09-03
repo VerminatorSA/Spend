2c676bae19d014100f6a1c641cbf243c1f57046b
Admin Verminator <trust@verminator.co.za>
2025-09-03 09:30:08 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,17 +4,17 @@ import { getAuth } from 'firebase/auth';
 import { getFunctions, httpsCallable } from 'firebase/functions';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "spend-5vu0f.firebaseapp.com",
   projectId: "spend-5vu0f",
-  storageBucket: "spend-5vu0f.firebasestorage.app",
-  messagingSenderId: "1068565293615",
   appId: "1:1068565293615:web:a2f86e579ccad72c1334da",
+  storageBucket: "spend-5vu0f.firebasestorage.app",
+  apiKey: "AIzaSyD88tMZfM9lG5K2gAgvEOBoMS4gTjS4VF0",
+  authDomain: "spend-5vu0f.firebaseapp.com",
+  messagingSenderId: "1068565293615",
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
-const functions = getFunctions(app);
+const functions = getFunctions(app, 'us-central1');
 
 export { app, auth, functions, httpsCallable };
